b204d19d4655732c638777497e4dd447994b998a
Daniel Milewski <daniel.milewski@jit.team>
2024-04-14 14:08:29 +0200

    Create example wagtail project

---
+++ mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-*s!n+fepm_ei0lk!)suau9dpun_d3%5ne!&pm7_&co6yp0497="
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
